c42df8e035b10ad82825ae57c8141c4d9bb7e29a
neno2000 <duran.ernesto@gmail.com>
2018-12-17 07:32:40 +0100

    routing added

---
+++ public/controller/App.controller.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/demo/basicTemplate/model/formatter"
+], function(Controller, JSONModel, formatter) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.basicTemplate.controller.Main", {
+
+		formatter: formatter,
+
+		onInit: function () {
+		},
+
+		onResett: function(oEvent) {
+			// get the field and resett the values
+			var oAnvId = this.byId("anvId");
+			var oForId = this.byId("forId");
+			var oTjaId = this.byId("tjaId");
+			var oDatId = this.byId("datId");
+			var oInAvtId = this.byId("inAvtId");
+			// resett the values
+			oAnvId.setValue();
+			oForId.setValue();
+	  	oTjaId.setValue();
+			oDatId.setValue();
+			oInAvtId.setSelected();
+		},
+		onSearch: function(oEvent) {
+			// get fields values
+			// route to next window
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+      oRouter.navTo("detail");
+		}
+	});
+});
--- public/controller/Main.controller.js
+++ public/controller/Main.controller.js
@@ -5,34 +5,11 @@ sap.ui.define([
 ], function(Controller, JSONModel, formatter) {
 	"use strict";
 
-	return Controller.extend("sap.ui.demo.basicTemplate.controller.Main", {
-
-		formatter: formatter,
+	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
 
 		onInit: function () {
-
-
 		},
 
-		onResett: function(oEvent) {
-			// get the field and resett the values
-			var oAnvId = this.byId("anvId");
-			var oForId = this.byId("forId");
-			var oTjaId = this.byId("tjaId");
-			var oDatId = this.byId("datId");
-			var oInAvtId = this.byId("inAvtId");
-			// resett the values
-			oAnvId.setValue();
-			oForId.setValue();
-	  	oTjaId.setValue();
-			oDatId.setValue();
-			oInAvtId.setSelected();
-		},
-		onSearch: function(oEvent) {
-			// get fields values
-			// route to next window
-			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-      oRouter.navTo("detail");
-		}
+
 	});
 });
